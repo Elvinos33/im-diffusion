844f0628374727d1b1e2135f1f77fb618b9d9a50
Elvin Baloku <elvinos33@duck.com>
2023-09-12 13:58:09 +0200

    Half-assed

--- src/app/generate/page.tsx
+++ src/app/generate/page.tsx
@@ -1,15 +1,22 @@
+"use client";
+
 import Image from "next/image"
+import AiForm from "@/components/aiForm"
+import {useState} from "react";
 
 export default function Page() {
+
+    const [image, setImage] = useState("")
+
     return (
         <div className="w-screen flex-col items-center flex">
             <main className="flex w-screen flex-col h-screen items-center gap-1">
-                <section className="  flex w-full h-1/4 items-center justify-end flex-col gap-2">
-                    <input placeholder="Skriv her min venn :)" className="h-10 w-3/6 rounded-lg p-2 text-black" type="text" />
-                    <button className="bg-violet-500 text-white w-1/6 h-10 rounded-lg">Generate</button>
+                <span className={"py-10 text-6xl font-bold"}>STAI_AI_Kaptein</span>
+                <section className="flex w-full h-1/4 items-center justify-end flex-col gap-2">
+                    <AiForm setImageData={setImage}/>
                 </section>
                 <section className="mt-12 w-4/6 items-center justify-center">
-                        <Image alt="AI bilde" src="/images/1.png" width={500} height={500} />
+                        <Image alt="AI bilde" src={image} width={500} height={500} />
                 </section>
             </main>
             <section className="flex bg-gray-700 w-3/4 h-screen">
--- src/components/aiForm.tsx
+++ src/components/aiForm.tsx
@@ -1,12 +1,12 @@
 import axios from "axios";
 import {useForm} from "react-hook-form";
-import {useState} from "react";
+import {useEffect, useState} from "react";
 
 type Props = {
     setImageData(): void
 }
 
-export default function aiForm(props:Props) {
+export default function AiForm(props:Props) {
 
     const {handleSubmit, reset, register} = useForm()
 
@@ -19,49 +19,55 @@ export default function aiForm(props:Props) {
     function onSubmit(data: any) {
 
         const requestData = {
-            "prompt": data.prompt,
-            "seed": getRandomNumber(1000, 100000000),
-            "used_random_seed": true,
-            "negative_prompt": "(deformed iris, deformed pupils, semi-realistic, cgi, 3d, render, sketch, cartoon, drawing, anime:1.4), text, close up, cropped, out of frame, worst quality, low quality, jpeg artifacts, ugly, duplicate, morbid, mutilated, extra fingers, mutated hands, poorly drawn hands, poorly drawn face, mutation, deformed, blurry, dehydrated, bad anatomy, bad proportions, extra limbs, cloned face, disfigured, gross proportions, malformed limbs, missing arms, missing legs, extra arms, extra legs, fused fingers, too many fingers, long neck,",
-            "num_outputs": 1,
-            "num_inference_steps": 35,
-            "guidance_scale": 7,
-            "width": 512,
-            "height": 512,
-            "vram_usage_level": "high",
-            "sampler_name": "euler_a",
-            "use_stable_diffusion_model": "DreamShaper",
-            "clip_skip": false,
-            "use_vae_model": "",
-            "stream_progress_updates": true,
-            "stream_image_progress": false,
-            "show_only_filtered_image": true,
-            "block_nsfw": true,
-            "output_format": "jpeg",
-            "output_quality": 75,
-            "output_lossless": false,
-            "metadata_output_format": "none",
-            "original_prompt": data.prompt,
-            "active_tags": [],
-            "inactive_tags": [],
-            "save_to_disk_path": "/home/ai/Stable Diffusion UI",
-            "use_upscale": "RealESRGAN_x4plus",
-            "upscale_amount": "4"
+            prompt: data.prompt,
+            seed: getRandomNumber(1000, 100000000),
+            used_random_seed: true,
+            negative_prompt: "(deformed iris, deformed pupils, semi-realistic, cgi, 3d, render, sketch, cartoon, drawing, anime:1.4), text, close up, cropped, out of frame, worst quality, low quality, jpeg artifacts, ugly, duplicate, morbid, mutilated, extra fingers, mutated hands, poorly drawn hands, poorly drawn face, mutation, deformed, blurry, dehydrated, bad anatomy, bad proportions, extra limbs, cloned face, disfigured, gross proportions, malformed limbs, missing arms, missing legs, extra arms, extra legs, fused fingers, too many fingers, long neck,",
+            num_outputs: 1,
+            num_inference_steps: 35,
+            guidance_scale: 7,
+            width: 512,
+            height: 512,
+            vram_usage_level: "high",
+            sampler_name: "euler_a",
+            use_stable_diffusion_model: "DreamShaper",
+            clip_skip: false,
+            use_vae_model: "",
+            stream_progress_updates: true,
+            stream_image_progress: false,
+            show_only_filtered_image: true,
+            block_nsfw: true,
+            output_format: "jpeg",
+            output_quality: 75,
+            output_lossless: false,
+            metadata_output_format: "none",
+            original_prompt: data.prompt,
+            active_tags: [],
+            inactive_tags: [],
+            save_to_disk_path: "/home/ai/Stable Diffusion UI",
+            use_upscale: "RealESRGAN_x4plus",
+            upscale_amount: "4"
         }
 
-        axios.post("10.58.176.142:9000/render", requestData)
+        axios.post("http://10.58.176.142:9000/render", requestData)
             .then(function (response) {
+                console.log(response)
                 setRenderData(response)
+                setTimeout(getImageData, 7000)
         })
-
-        setTimeout(getImageData, 7000)
+        reset();
     }
 
+    useEffect(() => {
+        console.log(renderData.data)
+    }, [renderData]);
+
     function getImageData() {
-        axios.get(`10.58.176.142:9000/image/stream/${renderData.task}`)
+        axios.get(`http://10.58.176.142:9000/image/stream/${renderData.data.task}`)
             .then((response) => {
                 console.log(response);
                 props.setImageData(response);
+                return
             })
             .catch((error) => {
                 console.error(error);
@@ -74,9 +80,9 @@ export default function aiForm(props:Props) {
 
     return (
         <>
-            <form onSubmit={handleSubmit(onSubmit)}>
-                <input type="text" placeholder={"Skriv her.."} {...register("prompt", {required: true})}/>
-                <button>Generer</button>
+            <form onSubmit={handleSubmit(onSubmit)} className={"flex flex-col gap-3 w-1/2"}>
+                <input type="text" className={"p-5 rounded-md text-black bg-slate-100 transition hover:bg-white focus:bg-white"} placeholder={"Skriv her.."} {...register("prompt", {required: true})}/>
+                <button className={"bg-slate-800 py-3 rounded-md font-bold transition hover:bg-slate-300 hover:text-black"}>Generer</button>
             </form>
         </>
     )
