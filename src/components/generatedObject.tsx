8b9af793f1db04b39231cebbdbdd6d75de60f431
Herjus <herjus420@gmail.com>
2023-09-16 14:12:45 +0200

    my bad

--- src/components/generatedObject.tsx
+++ src/components/generatedObject.tsx
@@ -13,8 +13,8 @@ export function GeneratedImageCard(props: GeneratedObject){
             </CardHeader>
             <CardContent>
                 {props.image ? (
-                    // <Image alt="AI bilde" className="w-full" src={`data:image/png;base64,${props.image}`}  width={500} height={500} />
-                    <img alt="AI bilde" className="w-full" src={`${props.image}`} />
+                    <Image alt="AI bilde" className="w-full" src={`data:image/png;base64,${props.image}`}  width={500} height={500} />
+                    // <img alt="AI bilde" className="w-full" src={`${props.image}`} />
                 ) : (
                     <Skeleton className="w-full aspect-square"/>
                 )}
